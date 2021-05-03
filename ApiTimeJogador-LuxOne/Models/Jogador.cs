29dc55dfa16f5cd5db49b7d3dd29449002925274
eriksena16 <eriksena16@gmail.com>
2021-05-03 13:50:23 -0300

    Classe Jogador

---
+++ ApiTimeJogador-LuxOne/Models/Jogador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTimeJogador_LuxOne.Models
+{
+    public class Jogador
+    {
+        public int JogadorID { get; set; }
+        public string NomeCompleto { get; set; }
+        public int Idade { get; set; }
+        public int TimeID { get; set; }
+
+        public Time Time { get; set; }
+    }
+}
