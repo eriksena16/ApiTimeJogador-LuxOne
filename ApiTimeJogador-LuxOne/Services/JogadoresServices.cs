2fe293dcf6fc98691660d8bf326121f800f2e06d
@eriksena16 <eriksena16@gmail.com>
2021-05-05 21:28:02 -0300

    Instaciamento do DTQ

--- ApiTimeJogador-LuxOne/Services/JogadoresServices.cs
+++ ApiTimeJogador-LuxOne/Services/JogadoresServices.cs
@@ -6,47 +6,36 @@ using System.Threading.Tasks;
 using ApiTimeJogador_LuxOne.Data;
 using Microsoft.EntityFrameworkCore;
 using ApiTimeJogador_LuxOne.Iterfaces;
+using ApiTimeJogador_LuxOne.Models.DTQ;
 
 namespace ApiTimeJogador_LuxOne.Services
 {
     public class JogadoresServices : IJogadoresService
     {
         private readonly APIcontext _context;
-        public JogadoresServices(APIcontext context)
-        {
-            _context = context;
-        }
-        public async Task<IEnumerable<Jogador>> Get()
-        {
-            return await _context.Jogadores.ToListAsync();
-        }
+        public JogadoresServices(APIcontext context) => _context = context;
+        public async Task<IEnumerable<Jogador>> Get() => await _context.Jogadores.ToListAsync();
 
         public async Task<Jogador> GetID(int id)
         {
             var jogador =  await _context.Jogadores.FindAsync(id);
             return jogador;
         }
-        public async Task<IEnumerable<Jogador>> BuscaJogadoresPorTime(int id)
-        {
-            return await _context.Jogadores.Where(_ => string.Equals(Convert.ToString(_.TimeID), Convert.ToString(id), StringComparison.OrdinalIgnoreCase)).ToListAsync();
-        }
+        public async Task<IEnumerable<Jogador>> BuscaJogadoresPorTime(int id) => await _context.Jogadores.Where(_ => string.Equals(Convert.ToString(_.TimeID), Convert.ToString(id), StringComparison.OrdinalIgnoreCase)).ToListAsync();
 
-        public async Task<IEnumerable<Jogador>> BuscaPorIdade(int idade)
-        {
+        public async Task<IEnumerable<Jogador>> BuscaPorIdade(int idade) => await _context.Jogadores.Where(_ => string.Equals(Convert.ToString(_.Idade), Convert.ToString(idade), StringComparison.OrdinalIgnoreCase)).ToListAsync();
 
-            return  await _context.Jogadores.Where(_ => string.Equals(Convert.ToString( _.Idade), Convert.ToString(idade), StringComparison.OrdinalIgnoreCase)).ToListAsync();
-        }
-
-        public async Task<Jogador> Salvar(Jogador jogador)
+        public async Task<Jogador> Salvar(JogadorSalvarDTQ jogadorSalvarQuery)
         {
+            Jogador jogador = new Jogador();
+            jogador.NomeCompleto = jogadorSalvarQuery.NomeCompleto;
+            jogador.Idade = jogadorSalvarQuery.Idade;
+            jogador.TimeID = jogadorSalvarQuery.TimeID;
+
             _context.Jogadores.Add(jogador);
             await _context.SaveChangesAsync();
             return await this.GetID(jogador.JogadorID);
         }
 
-         private bool JogadorExists(int id)
-        {
-            return _context.Jogadores.Any(e => e.JogadorID == id);
-        }
     }
 }
--- ApiTimeJogador-LuxOne/Services/TimesServices.cs
+++ ApiTimeJogador-LuxOne/Services/TimesServices.cs
@@ -1,37 +1,51 @@
 ﻿using ApiTimeJogador_LuxOne.Data;
+using ApiTimeJogador_LuxOne.Iterfaces;
 using ApiTimeJogador_LuxOne.Models;
+using ApiTimeJogador_LuxOne.Models.DTO;
+using ApiTimeJogador_LuxOne.Models.DTQ;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ApiTimeJogador_LuxOne.Iterfaces;
-using ApiTimeJogador_LuxOne.Models.DTO;
 
 namespace ApiTimeJogador_LuxOne.Services
 {
     public class TimesServices : ITimesService
     {
         private readonly APIcontext _context;
-        public TimesServices( APIcontext context)
-        {
-            _context = context;
-        }
+        public TimesServices(APIcontext context) => _context = context;
 
         public async Task<List<TimeDTO>> Get()
         {
-
             var todosTimes = _context.Times.Include(_ => _.Jogadores);
             List<TimeDTO> times = new List<TimeDTO>();
 
             foreach (Time time in todosTimes)
             {
-                var mediaIdade = time.Jogadores.Average(_ => _.Idade);
+
+                double? mediaIdade = null;
+                List<JogadorDTO> jogadores = new List<JogadorDTO>();
+                if (time.Jogadores.Any())
+                {
+                    mediaIdade = time.Jogadores.Average(_ => _.Idade);
+
+                    foreach (Jogador jogador in time.Jogadores)
+                    {
+                        JogadorDTO jogadorDTO = new JogadorDTO();
+
+                        jogadorDTO.JogadorID = jogador.JogadorID;
+                        jogadorDTO.NomeCompleto = jogador.NomeCompleto;
+                        jogadorDTO.Idade = jogador.Idade;
+                        jogadorDTO.TimeID = jogador.TimeID;
+                        jogadores.Add(jogadorDTO);
+                    }
+                }
+
                 TimeDTO timeDTO = new TimeDTO();
                 timeDTO.TimeID = time.TimeID;
                 timeDTO.Nome = time.Nome;
                 timeDTO.DataInclusao = time.DataInclusao;
-                timeDTO.Jogadores = time.Jogadores;
+                timeDTO.Jogadores = jogadores;
                 timeDTO.MediaIdadeJogadores = mediaIdade;
                 times.Add(timeDTO);
             }
@@ -40,25 +54,25 @@ namespace ApiTimeJogador_LuxOne.Services
 
         }
 
-        public async Task<Time> GetID( int id)
+        public async Task<Time> GetID(int id)
         {
             var time = _context.Times.Include(_ => _.Jogadores).FirstOrDefault(_ => _.TimeID == id);
             return time;
         }
 
-        public async Task<Time> Salvar(Time time)
+        public async Task<Time> Salvar(TimeSalvarDTQ timeSalvarQuery)
         {
+            Time time = new Time();
+
+            time.Nome = timeSalvarQuery.Nome;
+            time.DataInclusao = timeSalvarQuery.DataInclusao;
 
             _context.Times.Add(time);
             await _context.SaveChangesAsync();
             return await this.GetID(time.TimeID);
-            
+
         }
 
 
-         private bool TimeExists(int id)
-        {
-            return _context.Times.Any(e => e.TimeID == id);
-        }
     }
 }
