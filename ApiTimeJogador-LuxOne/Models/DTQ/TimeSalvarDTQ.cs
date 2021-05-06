92ba4591394d06b658cb4213525f5e8331fffce9
@eriksena16 <eriksena16@gmail.com>
2021-05-05 21:26:02 -0300

    Criação do DTQ

---
+++ ApiTimeJogador-LuxOne/Models/DTQ/TimeSalvarDTQ.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiTimeJogador_LuxOne.Models.DTQ
+{
+    public class TimeSalvarDTQ
+    {
+        public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataInclusao { get; set; }
+    }
+}
---
+++ ApiTimeJogador-LuxOne/Models/DTQ/jogadorSalvarDTQ.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTimeJogador_LuxOne.Models.DTQ
+{
+    public class JogadorSalvarDTQ
+    {
+        public string NomeCompleto { get; set; }
+
+        public int Idade { get; set; }
+
+        public int TimeID { get; set; }
+    }
+}
