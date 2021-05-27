12f996f763e0c0f490a1bdf1bd3831f523dd0cb9
@eriksena16 <eriksena16@gmail.com>
2021-05-26 23:30:37 -0300

    .

--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using LuxOne.Contrato.GatewayContract;
+using LuxOne.Service.GatewayService;
 
 namespace ApiTimeJogador_LuxOne
 {
